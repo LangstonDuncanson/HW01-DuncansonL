772a4028aaa2d225766533157215ea3bcb5f7adb
Langston Duncanson <landun@Langstons-iMac.local>
2017-11-13 01:57:53 -0500

    Initial Commit

---
+++ HW01-DuncansonL/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HW01-DuncansonL
+//
+//  Created by Langston Duncanson on 11/13/17.
+//  Copyright © 2017 Langston Duncanson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HW01-DuncansonLTests/HW01_DuncansonLTests.m
@@ -0,0 +1,39 @@
+//
+//  HW01_DuncansonLTests.m
+//  HW01-DuncansonLTests
+//
+//  Created by Langston Duncanson on 11/13/17.
+//  Copyright © 2017 Langston Duncanson. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface HW01_DuncansonLTests : XCTestCase
+
+@end
+
+@implementation HW01_DuncansonLTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
